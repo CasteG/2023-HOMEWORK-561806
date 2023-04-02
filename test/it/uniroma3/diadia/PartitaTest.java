ea4edd6718722e6e17445624d6371fd86b60f3f3
CasteG <114982289+CasteG@users.noreply.github.com>
2023-04-02 11:51:59 +0200

    Add files via upload

---
+++ test/it/uniroma3/diadia/PartitaTest.java
@@ -0,0 +1,54 @@
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.uniroma3.diadia.ambienti.Stanza;
+
+public class PartitaTest {
+	
+	private Partita partitaTest;
+	
+	@Before
+	public void setUp() {
+		this.partitaTest = new Partita();
+	}
+
+	@Test
+	public void testVinta() {
+		this.partitaTest.setStanzaCorrente(this.partitaTest.getLabirinto().getStanzaVincente());
+		assertTrue(this.partitaTest.vinta());
+	}
+	
+	@Test
+	public void testVintaSeStanzaCorrenteDiversaDaVincente() {
+		this.partitaTest.setStanzaCorrente(new Stanza("N1"));
+		assertFalse(this.partitaTest.vinta());
+	}
+	
+	@Test
+	public void testVintaSeAppenaIniziata() {
+		assertFalse(this.partitaTest.vinta());
+	}
+	
+
+	@Test
+	public void testIsFinitaSeCfuTerminati() {
+		this.partitaTest.getGiocatore().setCfu(0);
+		assertTrue(this.partitaTest.isFinita());
+	}
+	
+	@Test
+	public void testIsFinitaSeVinta() {
+		this.partitaTest.setStanzaCorrente(this.partitaTest.getLabirinto().getStanzaVincente());		
+		assertTrue(this.partitaTest.isFinita());
+	}
+	
+	@Test
+	public void testIsFinitaSeAppenaIniziata() {
+		assertFalse(this.partitaTest.isFinita());
+	}
+	
+}
